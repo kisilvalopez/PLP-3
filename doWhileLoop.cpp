8f82b1666afc0d3777d335ed75729833a2cce311
Kimberly Lopez <144376132+kisilvalopez@users.noreply.github.com>
2023-11-29 15:23:45 -0500

    Create doWhileLoop.cpp

---
+++ doWhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    //initialize our expressions
+    int x = 1;
+    do{
+        cout << "Hello World!\n";
+        x++;
+    //as long as x is less than 5, we will see that the output is "Hello World" x 4
+    } while (x < 5);
+
+    return 0;
+}
