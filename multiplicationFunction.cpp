c0387497d3712dad1e0741f07613e4dc10be4f2e
Kimberly Lopez <144376132+kisilvalopez@users.noreply.github.com>
2023-11-29 15:28:13 -0500

    Create multiplicationFunction.cpp

---
+++ multiplicationFunction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int num1, num2, total;
+    cout << "Enter two numbers: ";
+    //stores num1 and num2
+    cin >> num1 >> num2;
+    //multiplication process and stores the result in total
+    total = num1 * num2;
+    cout << "Total: " << total;
+    return 0;
+}
