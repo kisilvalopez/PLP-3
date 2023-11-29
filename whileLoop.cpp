d56372c7b49c5a472361209b6ff7b0daccd94e9f
Kimberly Lopez <144376132+kisilvalopez@users.noreply.github.com>
2023-11-29 15:22:53 -0500

    Create whileLoop.cpp

---
+++ whileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    //we initialize our expression
+    int x = 1;
+    //create our while loop and test the expression
+    while (x < 5) {
+        cout << "Hello World!\n";
+        x++;
+
+    }
+    return x;
+}
